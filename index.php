5dc11962eb316bec06c53daa8b1c1b9cc515f106
rakib3joy <rakibhasanjoy286@gmail.com>
2024-08-25 00:27:14 +0600

    delete

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP tutorial</title>
+</head>
+<body>
+    <div class = "container">
+        This is my first php website
+        <?php
+            echo "Hello world and this is printed using php";
+            //sinlge line comment
+            /*
+
+
+                This is multi
+                line
+                comment
+            */
+
+            //php no case sensitive
+
+        $variable1  = 34;
+        $variable2 = 45; 
+        echo $variable1;
+        echo $variable2;   
+
+        echo $variable1 + $variable2;
+        
+        //operators in php
+
+        // Arithmetic operators
+        
+        // Assignment operators
+        // Comparison operators
+        // Increment/Decrement operators
+        // Logical operators
+
+
+
+
+
+
+        ?>  
+        <?php
+          echo "Hello world again";
+        
+        ?>
+        </div>  
+    
+    
+</body>
+</html>
